6682af6c6b812364ad6c76e78baaea45e29d5b91
Peter Lyons <pete@peterlyons.com>
2015-03-17 12:12:45 -0600

    countTypes.stream is working

--- scanMongodb.js
+++ scanMongodb.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 var countTypesStream = require("./countTypes").stream();
+var DevNull = require("dev-null-stream");
 var mongodbFootman = require("./footmen/mongodb");
 var rules = require("./rules");
 
 function onEnd() {
+  console.log("----- Results -----");
   console.log(rules(countTypesStream.stats));
-  console.log("done", countTypesStream.stats);
+  console.log("----- Stats -----");
+  console.log(countTypesStream.stats);
 }
 
 mongodbFootman(process.argv[2], process.argv[3], function (error, mongoStream) {
@@ -17,7 +20,5 @@ mongodbFootman(process.argv[2], process.argv[3], function (error, mongoStream) {
   countTypesStream
     .on("error", console.error)
     .on("end", onEnd)
-    .on("data", function (doc) {
-      console.log("@bug countTypesStream data", doc);
-    });
+    .pipe(new DevNull());
 });
