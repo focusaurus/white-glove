da997d79d0f0f6a55d741a32c2c7ba7b61ed9c0a
Peter Lyons <pete@peterlyons.com>
2015-03-17 09:35:50 -0600

    refactor results and consistent types

--- rules/consistentTypes.js
+++ rules/consistentTypes.js
@@ -1,28 +1,14 @@
 var _ = require("lodash");
 
-function check(stats) {
-  var paths = [];
+function check(stats, results) {
   for (var keyPath in stats) {
     var typeCounts = stats[keyPath];
     var types = _.keys(typeCounts);
     types = _.without(types, "null");
     if (types.length > 1) {
-      paths.push(keyPath);
+      results.warning("Inconsistent Types", {keyPath: keyPath, types: types});
     }
   }
-  return paths;
 }
 
 module.exports = check;
-
-
-// function findTypeMismatches(stats) {
-//   var mismatches = [];
-//   for (var keyPath in stats) {
-//     var typeCounts = stats[keyPath];
-//     if (_.size(typeCounts) > 1) {
-//       mismatches.push(keyPath);
-//     }
-//   }
-//   return mismatches;
-// }
--- rules/consistentTypes.test.js
+++ rules/consistentTypes.test.js
@@ -2,15 +2,35 @@ var _ = require("lodash");
 var test = require("tape");
 var countTypes = require("../countTypes");
 var consistentTypes = require("./consistentTypes");
+var Results = require("./Results");
 
 test("rules/consistentTypes should find paths", function(assert) {
-  var input1 = {one: true, two: false, three: []};
-  var input2 = {one: false, two: true, three: []};
-  var input3 = {one: false, two: "false", three: []};
+  var input1 = {
+    one: true,
+    two: false,
+    three: []
+  };
+  var input2 = {
+    one: false,
+    two: true,
+    three: []
+  };
+  var input3 = {
+    one: false,
+    two: "false",
+    three: []
+  };
   var stats = {};
+  var results = new Results();
   countTypes(stats, null, input1);
   countTypes(stats, null, input2);
   countTypes(stats, null, input3);
-  assert.equal(_.difference(consistentTypes(stats), ["two"]).length, 0);
+  consistentTypes(stats, results);
+  var expected = [{
+    message: "Inconsistent Types",
+    keyPath: "two",
+    types: ["boolean", "string"]
+  }];
+  assert.deepEqual(results.warnings, expected);
   assert.end();
 });
